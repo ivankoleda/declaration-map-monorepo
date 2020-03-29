d5860e1d39d3381a1470f0dfab8a0eda1814cdcf
Ivan Koleda <ikoleda@aristeksystems.com>
2020-03-29 12:01:11 +0300

    update comments

--- packages/app/src/index.tsx
+++ packages/app/src/index.tsx
@@ -2,13 +2,13 @@ import { render } from "react-dom";
 import React from "react";
 import { sum, Button } from "shared";
 
-// CMD + Click will get to source code
+// CMD + Click will get to .ts file
 sum(1, 2);
 
 render(
   <div>
     App that uses shared  components package
-    {/* // CMD + Click will get to declaration */}
+    {/* // CMD + Click will get to to .d.ts file */}
     <Button />
   </div>,
   document.getElementById("root")
